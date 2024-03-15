27724ba64f343a86070598f1ca4422341566df4a
Server <gri.e.p@ya.ru>
2024-03-15 12:53:30 +0000

    counter

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,22 +21,6 @@ function Header(props) {
 
   return (
     <header className="header">
-      <!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();
-   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-   ym(39084735, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true
-   });
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/39084735" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
       <Link to="/" className="header__link">
         <img src={logo} alt="logo" className="header__logo"  />
       </Link>
